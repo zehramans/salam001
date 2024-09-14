e30d599dd208a5c45c3284339104bc471befb1ab
zehramans <zehramansirova0@gmail.com>
2024-09-14 12:16:19 +0400

    Create wallpaper.ps1

---
+++ wallpaper.ps1
@@ -0,0 +1,21 @@
+Invoke-WebRequest -Uri "https://media.makeameme.org/created/you-have-been-5b63ed.jpg" -OutFile $env:USERPROFILE\Downloads\download.jpg
+$imgPath="$env:USERPROFILE\Downloads\download.jpg"
+$code = @' 
+using System.Runtime.InteropServices; 
+namespace Win32{ 
+    
+     public class Wallpaper{ 
+        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+         
+         public static void SetWallpaper(string thePath){ 
+            SystemParametersInfo(20,0,thePath,3); 
+         }
+    }
+ } 
+'@
+
+add-type $code 
+
+#Apply the Change on the system 
+[Win32.Wallpaper]::SetWallpaper($imgPath)
